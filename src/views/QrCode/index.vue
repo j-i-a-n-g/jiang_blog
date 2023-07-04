70b56f233c6166d3e518dfe2cbbe641e76bb0517
李江 <1280291927@qq.com>
2023-07-04 23:23:23 +0800

    生成二维码页面实现

--- src/views/QrCode/index.vue
+++ src/views/QrCode/index.vue
@@ -1,13 +1,97 @@
 <template>
-  
+  <div class="qrCode">
+    <TopicTitle topic="制作二维码" iconClass="el-icon-document" />
+    <div class="main">
+      <div class="left">
+        <Setting :formData="formData" />
+      </div>
+      <div class="right">
+        <vue-qr 
+        :text="formData.qrText"
+        :correctLevel="formData.qrCorrectLevel"
+        :size="formData.qrSize"
+        :margin="formData.qrMargin"
+        :colorDark="formData.qrColorDark"
+        :colorLight="formData.qrColorLight"
+        :bgSrc="formData.qrBgSrc"
+        :gifBgSrc="formData.qrGifBgSrc"
+        :backgroundColor="formData.qrBackgroundColor"
+        :logoSrc="formData.qrLogoSrc"
+        :logoScale="formData.logoScale"
+        :logoMargin="formData.qrLogoMargin"
+        :logoBackgroundColor="formData.qrLogoBackgroundColor"
+        :logoCornerRadius="formData.qrLogoCornerRadius"
+        :whiteMargin="formData.qrWhiteMargin"
+        :dotScale="formData.qrDotScale"
+        ></vue-qr>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import TopicTitle from '@/components/TopicTitle.vue'
+import Setting from './setting.vue'
+import VueQr from 'vue-qr'
 export default {
-
+  components: {TopicTitle, VueQr, Setting},
+  data() {
+    return {
+      formData: {
+        qrText: "hello World",
+        qrCorrectLevel: 0, // 0-3
+        qrSize: 120, // 尺寸，长宽一致
+        qrMargin: 20, // 二维码外边距
+        qrColorDark: "#000", // 二维码实点颜色
+        qrColorLight: "#fff", // 二维码空白区颜色
+        qrBgSrc: null, // 要嵌入的背景图地址
+        qrGifBgSrc: null, // 要嵌入的Gif背景图地址
+        qrBackgroundColor: "#fff",// 背景色
+        qrBackgroundDimming: "#ccc",//叠加在背景图上的颜色
+        qrLogoSrc: null,// logo图
+        qrLogoScale: 0.2, // logo大小
+        qrLogoMargin: 0, // logo周围的留白
+        qrLogoBackgroundColor: "transparent", // logo的背景颜色
+        qrLogoCornerRadius: 0, //logo边框的圆角半径
+        qrWhiteMargin: true, // 背景图外将绘制白色边框
+        qrDotScale: 1, //数据区域点缩小
+      }
+    }
+  },
+  methods: {
+    savingSetting(data) {
+      this.formData = data;
+    }
+  }
 }
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+.qrCode {
+  padding: 20px;
+  width: 100%;
+  box-sizing: border-box;
+}
+.main {
+  height: 100%;
+  display: flex;
+  .left {
+    height: calc(100vh - 120px);
+    background-color: rgba(225, 225, 225, .2);
+    overflow: scroll;
+    flex: 2;
+  }
+  .right {
+    flex: 1;
+    border-left: 1px solid #ccc;
+    text-align: center;
+    height: calc(100vh - 120px);
+    box-sizing: border-box;
+    padding-top: 40px;
+    background-color: #fff;
+    img {
+      border: 1px solid #000;
+    }
+  }
+}
 </style>
---
+++ src/views/QrCode/setting.vue
@@ -0,0 +1,178 @@
+<template>
+  <div class="setting-qr">
+    <el-form ref="form" :model="form" label-width="120px">
+      <el-form-item label="二维码内容">
+        <el-input v-model="form.qrText"></el-input>
+      </el-form-item>
+      <el-form-item label="二维码尺寸">
+        <el-input-number
+        size="mini"
+          v-model="form.qrSize"
+          :min="60"
+          :max="300"
+        ></el-input-number>
+      </el-form-item>
+      <el-form-item label="容错级别">
+        <el-input-number
+        size="mini"
+          v-model="form.qrCorrectLevel"
+          :min="0"
+          :max="3"
+        ></el-input-number>
+      </el-form-item>
+      <el-form-item label="码外边距">
+        <el-input-number
+        size="mini"
+          v-model="form.qrMargin"
+          :step="5"
+          :min="0"
+          :max="100"
+        ></el-input-number>
+      </el-form-item>
+      <el-row>
+        <el-col :span="12">
+          <el-form-item label="实点颜色">
+            <el-color-picker
+              v-model="form.qrColorDark"
+              show-alpha
+            ></el-color-picker>
+          </el-form-item>
+        </el-col>
+        <el-col :span="12">
+          <el-form-item label="空白区颜色">
+            <el-color-picker
+              v-model="form.qrColorLight"
+              show-alpha
+            ></el-color-picker>
+          </el-form-item>
+        </el-col>
+      </el-row>
+      <el-row>
+        <el-col :span="12">
+          <el-form-item label="背景色">
+            <el-color-picker
+              v-model="form.qrBackgroundColor"
+              show-alpha
+            ></el-color-picker>
+          </el-form-item>
+        </el-col>
+        <el-col :span="12">
+          <el-form-item label="背景图叠加色">
+            <el-color-picker
+              v-model="form.qrBackgroundDimming"
+              show-alpha
+            ></el-color-picker>
+          </el-form-item>
+        </el-col>
+      </el-row>
+      <el-form-item label="背景图地址">
+        <el-input v-model="form.qrBgSrc"></el-input>
+      </el-form-item>
+      <el-form-item label="gif背景图地址">
+        <el-input v-model="form.qrGifBgSrc"></el-input>
+      </el-form-item>
+      <el-form-item label="logo图地址">
+        <el-input v-model="form.qrLogoSrc"></el-input>
+      </el-form-item>
+      <el-row>
+        <el-col :span="12">
+          <el-form-item label="logo大小">
+            <el-input-number
+            size="mini"
+              v-model="form.qrLogoScale"
+              :step="0.1"
+              :min="0"
+              :max="1"
+            ></el-input-number>
+          </el-form-item>
+        </el-col>
+        <el-col :span="12">
+          <el-form-item label="logo圆角">
+            <el-input-number
+            size="mini"
+              v-model="form.qrLogoCornerRadius"
+              :step="1"
+              :min="0"
+            ></el-input-number>
+          </el-form-item>
+        </el-col>
+      </el-row>
+      <el-row>
+        <el-col :span="12">
+          <el-form-item label="logo背景颜色">
+            <el-color-picker
+              v-model="form.qrLogoBackgroundColor"
+              show-alpha
+            ></el-color-picker>
+          </el-form-item>
+        </el-col>
+        <el-col :span="12">
+          <el-form-item label="logo边留白">
+            <el-input-number
+            size="mini"
+              v-model="form.qrLogoMargin"
+              :step="2"
+              :min="0"
+              :max="30"
+            ></el-input-number>
+          </el-form-item>
+        </el-col>
+      </el-row>
+      <el-row>
+        <el-col :span="12">
+          <el-form-item label="外层白色边框">
+            <el-switch v-model="form.qrWhiteMargin"></el-switch>
+          </el-form-item>
+        </el-col>
+        <el-col :span="12">
+          <el-form-item label="数据区域点缩小">
+            <el-input-number size="mini" v-model="form.qrDotScale"></el-input-number>
+          </el-form-item>
+        </el-col>
+      </el-row>
+    </el-form>
+    <div class="btn-group">
+      <el-button type="primary" @click="onSubmit">生成二维码</el-button>
+      <el-button>取消</el-button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    formData: {
+      require: true,
+    },
+  },
+  data() {
+    return {
+      form: this.formData
+    };
+  },
+  methods: {
+    onSubmit() {
+      this.$emit("savingSetting", this.form)
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.setting-qr {
+  padding: 60px 10px 10px;
+  margin-top: 20px;
+  position: relative;
+}
+.setting-qr ::v-deep .el-form-item {
+  margin-bottom: 10px !important;
+  & .el-form-item__label {
+    color: #fff !important;
+  }
+}
+.btn-group {
+  position: absolute;
+  top: 0;
+  right: 20px;
+}
+</style>
